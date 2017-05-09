03ec228c3c7cc006f1d016704475482222a76e74
Kei Hibino <ex8k.hibino@gmail.com>
2017-05-09 15:17:53 +0900

    update exercise of monoid-error.

--- exercise/MonoidError.hs
+++ exercise/MonoidError.hs
@@ -4,6 +4,8 @@ import Data.Monoid
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Except
 
+import Calc (Expr (..), eval)
+
 
 -- :i Last
 -- Last Nothing <> Last (Just "foo")
@@ -33,3 +35,46 @@ token = undefined
 -- success on end of input, otherwise failure
 eof :: Parser ()
 eof = undefined
+
+
+-- 以下の文法の Expr型の結果を持つ parser を実装してください
+-- Implement following syntax parser which result is Expr type.
+-- 演算子は右結合でよい
+-- right associative operator implementation may be more simple.
+
+--  expr :=
+--       | expr + expr
+--       | expr * expr
+--       | ( expr )
+--       | <decimal number>
+
+readNum :: String -> Expr
+readNum = undefined
+
+-- decimal number parser
+numExpr :: Parser Expr
+numExpr = undefined
+
+-- unit term parser
+unitExpr :: Parser Expr
+unitExpr = undefined
+
+-- multiply formula parser
+multExpr :: Parser Expr
+multExpr = undefined
+
+-- plus formula parser
+plusExpr :: Parser Expr
+plusExpr = undefined
+
+-- top-level parser
+expr :: Parser Expr
+expr = undefined
+
+-- runParser expr "1"
+-- runParser expr "(1)"
+-- runParser expr "1+2+3"
+-- runParser expr "2*3*4"
+-- runParser expr "(1+2)*(3+4)+5"
+
+-- eval . fst <$> runParser expr "(1+2)*(3+4)+5"

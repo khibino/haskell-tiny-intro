4d4a66ee1021595034f41abb5824b3903850d136
Kei Hibino <ex8k.hibino@gmail.com>
2017-05-09 12:22:50 +0900

    update comment of monoid-error.

--- exercise/MonoidError.hs
+++ exercise/MonoidError.hs
@@ -5,6 +5,11 @@ import Control.Monad.Trans.State
 import Control.Monad.Trans.Except
 
 
+-- :i Last
+-- Last Nothing <> Last (Just "foo")
+-- Last (Just "foo") <> Last Nothing
+-- Last (Just "foo") <> Last (Just "bar")
+
 --  Monoid e                =>  MonadPlus (ExceptT e m)
 
 type Parser' in' = StateT in' (Except (Last String))
@@ -13,6 +18,11 @@ type Parser = Parser' String
 
 -- error message 付きの Parser を実装する
 -- implementing Parser with error message
+
+-- error message 付きの失敗
+-- failure with error message
+errorP :: String -> Parser a
+errorP s = undefined
 
 -- 入力を一文字消費し、結果とする parser
 -- parser which consume one char input and that char is parser's result

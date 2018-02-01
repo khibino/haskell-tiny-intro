6c1c29ec7501640c0d45f51431a2a7b86b7b2687
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 11:23:54 +0900

    add note about IO.

---
+++ exercise/IO.hs
@@ -0,0 +1,25 @@
+module IO where
+
+--
+-- IO モナド
+--
+
+-- 外界に影響を与えるプログラムを書くには
+-- IO という Monad を使う
+--
+-- Monad なので do 文を利用することができる
+
+-- putStrLn :: String -> IO ()
+-- putStrLn "Hello world!"
+
+-- getLine :: IO String
+-- getLine
+
+-- readLn :: Read a => IO a
+-- readLn
+
+-----
+
+--
+-- 遅延 IO
+--

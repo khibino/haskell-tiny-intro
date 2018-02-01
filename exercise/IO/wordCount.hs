89ef31a4b0a9e4dd6a1e7c29d6a2c04010de9aff
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 12:18:34 +0900

    add wordCount exercise.

---
+++ exercise/IO/wordCount.hs
@@ -0,0 +1,15 @@
+
+-- length :: [a] -> Int
+
+-- 単語に区切る
+-- words :: String -> [String]
+
+
+-- getContents
+
+-- 標準入力を全て読み込み、
+-- 単語に区切ってその数をカウントし、
+-- 結果の数値を出力する
+main :: IO ()
+main = do
+  undefined

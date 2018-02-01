05c42d285390e740592a70cfb793187596b259c6
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 11:52:11 +0900

    add note about associative law and lazy IO.

--- exercise/IO.hs
+++ exercise/IO.hs
@@ -7,7 +7,21 @@ module IO where
 -- 外界に影響を与えるプログラムを書くには
 -- IO という Monad を使う
 --
+-- IO a
+--    ^ 結果
+-- 「IO を行なった結果として a が得られる」
+-- というように読める
+
 -- Monad なので do 文を利用することができる
+-- 結合法則も成り立っている
+--
+-- do A
+--    do B
+--       C
+-- ===
+-- do do A
+--       B
+--    C
 
 -- putStrLn :: String -> IO ()
 -- putStrLn "Hello world!"
@@ -23,3 +37,10 @@ module IO where
 --
 -- 遅延 IO
 --
+
+-- IO の結果が遅延する特別な IO
+
+-- getContents :: IO String
+
+-- -- type FilePath = String
+-- readFile :: FilePath -> IO String

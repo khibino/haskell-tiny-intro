f06c2aba25d0bf0f9538f931857b13e62c94f75e
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 14:08:33 +0900

    update note and template of reverseWord exercise.

--- exercise/IO/reverseWord.hs
+++ exercise/IO/reverseWord.hs
@@ -5,8 +5,14 @@ import System.Environment
 -- 単語に区切る
 -- words :: String -> [String]
 
+-- 単語列を一つの文字列に
+-- unwords :: [String] -> String
+
 -- 行に区切る
 -- lines :: String -> [String]
+
+-- 複数行を一つの文字列に
+-- unlines :: [String] -> String
 
 -- リストを逆順に
 -- reverse :: [a] -> [a]
@@ -14,7 +20,18 @@ import System.Environment
 -- getArgs :: IO [String]
 
 -- readFile :: FilePath -> IO String
--- print :: Show a => a -> IO ()
+-- putStrLn :: String -> IO ()
+-- putStr :: String -> IO ()
+
+-- map :: (a -> b) -> [a] -> [b]
+-- (.) :: (b -> c) -> (a -> b) -> (a -> c)
+-- mapM_ :: Monad m => (a -> m b) -> [a] -> IO ()
+
+-- do の中で let を使って変数定義できる
+--   do x <- readLn
+--      let y = 2 * x
+--      ...
+
 
 -- コマンドライン引数の最初のファイルの内容の全ての行について、
 -- 単語の順序を逆順にし、
@@ -25,4 +42,5 @@ main = do
   args <- getArgs
   case args of
     []    ->  undefined
-    x:xs  ->  undefined
+    x:xs  ->  do
+      undefined

956b97f27a0768daeadeefd88367a75e50a9dbf2
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 14:29:08 +0900

    update note and template of linesCalc exercise.

--- exercise/linesCalc.hs
+++ exercise/linesCalc.hs
@@ -1,3 +1,4 @@
+import Control.Applicative
 import System.Environment
 
 import Parser
@@ -11,6 +12,7 @@ import Calc
 -- getArgs :: IO [String]
 
 -- readFile :: FilePath -> IO String
+-- putStrLn :: String -> IO ()
 -- print :: Show a => a -> IO ()
 
 -- map :: (a -> b) -> [a] -> [b]
@@ -20,10 +22,25 @@ import Calc
 -- コマンドライン引数の最初のファイルの内容の全ての行について、
 -- その文字列を expr として解釈し、
 -- それぞれの評価結果を出力する
+-- 解釈が失敗した場合には Error と出力する
 -- コマンドライン引数が無いときにはなにもしない
+
+-- 注意:
+--   runParser expr           ではなく
+--   runParser (expr <* eof)  を使った方がよい
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     []    ->  undefined
-    x:xs  ->  undefined
+    x:_xs  ->  do
+      in' <- readFile x
+      undefined
+
+-- 文字列を expr として解釈し、
+-- 評価結果を出力する
+-- 解釈が失敗した場合には Error と出力する
+parseAndPrint :: String -> IO ()
+parseAndPrint s = case undefined of
+  Nothing -> undefined
+  Just e  -> undefined

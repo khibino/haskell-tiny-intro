e0eb95cb665882cced4c653930014542a6283257
Kei Hibino <ex8k.hibino@gmail.com>
2018-02-01 12:04:49 +0900

    add exercise to eval calc.

---
+++ exercise/evalCalc.hs
@@ -0,0 +1,17 @@
+import Parser
+import Calc
+
+-- runParser
+-- expr
+-- eval
+-- print
+
+-- 文字列を一行読み込み、
+-- その文字列を expr として解釈し、
+-- expr の評価結果を出力するプログラム
+main :: IO ()
+main = do
+  undefined
+  case undefined of
+    Nothing  ->  undefined
+    Just v   ->  undefined
